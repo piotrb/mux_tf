9c2c5467c0f259cb436e7deb9f5e5da1cae9eaa9
Piotr Banasik <piotr@jane.app>
2023-06-14 12:11:51 -0700

    Make the yaml hack optional if :unsafe_load is not present on YAML

--- lib/mux_tf/yaml_cache.rb
+++ lib/mux_tf/yaml_cache.rb
@@ -2,36 +2,39 @@
 
 require "yaml/store"
 
-module YAML
-  # Explaination from @h4xnoodle:
-  #
-  # Since ruby 3+ and psych 4+, the yaml loading became extra safe so the
-  # expired_at timestamp in the yaml cache is no longer parsing for whatever reason.
-  #
-  # Attempts were made with
-  #
-  # `@store = YAML::Store.new path`
-  # =>
-  # `@store = YAML::Store.new(path, { aliases: true, permitted_classes: [Time] })`
-  # to get it to work but that didn't help, so decided to just bypass the safe
-  # loading, since the file is controlled by us for the version checking.
-  #
-  # This is to override the way that psych seems to be loading YAML.
-  # Instead of using 'load' which needs work to permit the 'Time' class
-  # (which from above I tried that and it wasn't working so I decided to just
-  # bypass and use what it was doing before).
-  # This brings us back to the equivalent that was working before in that unsafe
-  # load was used before the psych upgrade.
-  #
-  # This change: https://my.diffend.io/gems/psych/3.3.2/4.0.0
-  # is the changes that 'cause the problem' and so I'm 'fixing it' by using the old equivalent.
-  #
-  # Maybe the yaml cache needs more work to have
-  # `YAML::Store.new(path, { aliases: true, permitted_classes: [Time] }) work.`
-  #
-  class << self
-    undef load # avoid a warning about the next line redefining load
-    alias load unsafe_load
+# Ruby 2.7's YAML doesn't have a :unsafe_load method .. so we skip all that
+if YAML.singleton_methods.include?(:unsafe_load)
+  module YAML
+    # Explaination from @h4xnoodle:
+    #
+    # Since ruby 3+ and psych 4+, the yaml loading became extra safe so the
+    # expired_at timestamp in the yaml cache is no longer parsing for whatever reason.
+    #
+    # Attempts were made with
+    #
+    # `@store = YAML::Store.new path`
+    # =>
+    # `@store = YAML::Store.new(path, { aliases: true, permitted_classes: [Time] })`
+    # to get it to work but that didn't help, so decided to just bypass the safe
+    # loading, since the file is controlled by us for the version checking.
+    #
+    # This is to override the way that psych seems to be loading YAML.
+    # Instead of using 'load' which needs work to permit the 'Time' class
+    # (which from above I tried that and it wasn't working so I decided to just
+    # bypass and use what it was doing before).
+    # This brings us back to the equivalent that was working before in that unsafe
+    # load was used before the psych upgrade.
+    #
+    # This change: https://my.diffend.io/gems/psych/3.3.2/4.0.0
+    # is the changes that 'cause the problem' and so I'm 'fixing it' by using the old equivalent.
+    #
+    # Maybe the yaml cache needs more work to have
+    # `YAML::Store.new(path, { aliases: true, permitted_classes: [Time] }) work.`
+    #
+    class << self
+      undef load # avoid a warning about the next line redefining load
+      alias load unsafe_load
+    end
   end
 end
 
