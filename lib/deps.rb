638635a55a583a5d0e7b10041fb2abc0fc0021ec
Piotr Banasik <piotr@jane.app>
2023-08-16 16:42:10 -0700

    Handle deps better, do a quick attempt first, and if needed, re-run with install (much slower and spams stdout)

--- lib/deps.rb
+++ lib/deps.rb
@@ -2,6 +2,15 @@
 
 require "bundler/inline"
 
-gemfile do
+dep_def = proc do
   gemspec(path: File.join(__dir__, ".."))
+end
+
+begin
+  gemfile(&dep_def)
+rescue Bundler::GemNotFound
+  gemfile(true) do
+    source "https://rubygems.org"
+    instance_exec(&dep_def)
+  end
 end
